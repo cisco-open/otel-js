16f00ddc06743601456a69a0170963715d8e7d5b
haddasb <haddas@epsagon.com>
2022-01-09 10:08:07 +0200

    add epsagon.init function that wrap the otel launcher

--- README.md
+++ README.md
@@ -6,12 +6,13 @@ Currently uses otel only.
 
 To test the launcher:
 1. verify you have docker installed and use the config.yaml in this repository to run the collector:
-Node: you should supply full path in -v argument:
+Note: you should supply full path in -v argument:
 docker run --rm -p 13133:13133 -p 14250:14250 -p 14268:14268 \
       -p 55678-55679:55678-55679 -p 4317:4317 -p 8888:8888 -p 9411:9411 \
               -v "${HOME}/YOUR_PATH/otel-js/test/config.yaml":/otel-local-config.yaml \
       --name otelcol otel/opentelemetry-collector \
       --config otel-local-config.yaml;
-2. run from the root: node test/app.ts
+2. compile from the root: tsc -p .
+3. run from the root: node build/test/app.js
 3. go to http://localhost:8081/ and verify you see "Hello World"
 4. check the collector, you should see a trace there.
--- src/tracing.ts
+++ src/tracing.ts
@@ -1,39 +1,56 @@
-const opentelemetry = require("@opentelemetry/sdk-node");
-const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
-const { registerInstrumentations } = require('@opentelemetry/instrumentation');
-const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
-const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
-const { context, propagation, trace } = require('@opentelemetry/api');
-const { BatchSpanProcessor} = require('@opentelemetry/sdk-trace-base');
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
-const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
+// import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
+import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
+import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
+import grpc = require('@grpc/grpc-js');
 
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 
-//create trace provider
-const provider = new NodeTracerProvider();
+export function init(_configData: any) {
+  const configData = _configData;
+  
+  diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+  
+  const metadata = new grpc.Metadata();
+  // For instance, an API key or access token might go here.
+  metadata.set('X-Epsagon-Token', configData.token);
 
-//option to create console exporter
-//const consoleExporter = new opentelemetry.tracing.ConsoleSpanExporter();
+  const collectorOptions = {
+    //serviceName: configData.appName || 'DEFAULT APPLICATION NAME',
+    url: configData.collectorURL,
+    // credentials: grpc.credentials.createSsl(),
+    // metadata,
+    // headers: {
+    //   'X-Epsagon-Token': `${configData.token}`,
+    // },
+  };
+  diag.info('configData: ', configData);
 
-//create grpc otlp exporter
-const traceExporter = new OTLPTraceExporter({
-  url: 'http://localhost:4317'
-  //optionally put headers
-});
 
-//put exported inside bacth processor
-//put batch processor inside provider
-provider.addSpanProcessor(new BatchSpanProcessor(traceExporter));
+  //create trace provider
+  const provider = new NodeTracerProvider();
 
-//register the provider
-provider.register(); //optinally supply contextManager and propagator
+  //option to create console exporter
+  //const consoleExporter = new opentelemetry.tracing.ConsoleSpanExporter();
 
-registerInstrumentations({
-    tracerProvider: provider, // optional, only if global TracerProvider shouldn't be used
-    //meterProvider: meterProvider, // optional, only if global MeterProvider shouldn't be used
-    instrumentations: [
-      getNodeAutoInstrumentations(),
-      //new HttpInstrumentation(),
-  ],
-});
+  //create grpc otlp exporter
+  const traceExporter = new OTLPTraceExporter(collectorOptions);
+
+  //put exported inside bacth processor
+  //put batch processor inside provider
+  provider.addSpanProcessor(new BatchSpanProcessor(traceExporter));
+
+  //register the provider
+  provider.register(); //optinally supply contextManager and propagator
+
+  registerInstrumentations({
+      tracerProvider: provider, // optional, only if global TracerProvider shouldn't be used
+      //meterProvider: meterProvider, // optional, only if global MeterProvider shouldn't be used
+      instrumentations: [
+        // getNodeAutoInstrumentations(),
+        new HttpInstrumentation(),
+    ],
+  });
+}
