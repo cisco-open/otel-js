385de8bca23b5e6c8b95d02525fb71838561ed15
Osher Vaknin <osher@epsagon.com>
2022-01-09 11:59:50 +0200

    Added prettier, eslint and fix most of warnings

--- src/tracing.ts
+++ src/tracing.ts
@@ -1,4 +1,18 @@
-// import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+/*
+ * Copyright The Cisco Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
 import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
 import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
@@ -7,12 +21,10 @@ import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
 import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
 import grpc = require('@grpc/grpc-js');
 
-
 export function init(_configData: any) {
   const configData = _configData;
-  
   diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
-  
+
   const metadata = new grpc.Metadata();
   // For instance, an API key or access token might go here.
   metadata.set('X-Epsagon-Token', configData.token);
@@ -27,7 +39,6 @@ export function init(_configData: any) {
     // },
   };
   diag.info('configData: ', configData);
-
 
   //create trace provider
   const provider = new NodeTracerProvider();
@@ -46,11 +57,11 @@ export function init(_configData: any) {
   provider.register(); //optinally supply contextManager and propagator
 
   registerInstrumentations({
-      tracerProvider: provider, // optional, only if global TracerProvider shouldn't be used
-      //meterProvider: meterProvider, // optional, only if global MeterProvider shouldn't be used
-      instrumentations: [
-        // getNodeAutoInstrumentations(),
-        new HttpInstrumentation(),
+    tracerProvider: provider, // optional, only if global TracerProvider shouldn't be used
+    //meterProvider: meterProvider, // optional, only if global MeterProvider shouldn't be used
+    instrumentations: [
+      // getNodeAutoInstrumentations(),
+      new HttpInstrumentation(),
     ],
   });
 }
