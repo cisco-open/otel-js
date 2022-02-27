78e6b52ad7f75089e90baf76961a499adff4a1aa
Osher Vaknin <osherv@cisco.com>
2022-02-27 16:07:53 +0200

    feat(system-tests): debugging system tests docker-compose

--- system-tests/server/server.js
+++ system-tests/server/server.js
@@ -22,59 +22,16 @@ ciscoTracing.init(userOptions);
 
 require('http');
 const express = require('express');
-const mongodb = require('mongodb');
 
 // Constants
 const PORT = 7080;
 const HOST = '0.0.0.0';
-
-const mongoUri = process.env.MONGO_URL;
-const dbName = 'store';
 
 // App
 const app = express();
 app.get('/', (req, res) => {
   res.status(200).send('Hallo Worldz');
 });
-
-app.get('/mongo', (req, res) => {
-  accessCollection(mongoUri, dbName, 'test')
-    .then(result => {
-      const collection = result.collection;
-      const insertData = [{ a: 1 }, { a: 2 }, { a: 3 }];
-      collection.insertMany(insertData, (err, result) => {
-        if (err) {
-          res.status(400).send('Command failed: ' + err);
-        } else {
-          res.status(200).send('Command success!');
-        }
-      });
-    })
-    .catch(err => {
-      res.status(501).send('Failed to connect mongo, err: ' + err);
-    });
-});
-
-/**
- * Access the mongodb collection.
- * @param url The mongodb URL to access.
- * @param dbName The mongodb database name.
- * @param collectionName The mongodb collection name.
- * @param options The mongodb client config options.
- */
-function accessCollection(url, dbName, collectionName, options) {
-  return new Promise((resolve, reject) => {
-    mongodb.MongoClient.connect(url, { serverSelectionTimeoutMS: 1000 })
-      .then(client => {
-        const db = client.db(dbName);
-        const collection = db.collection(collectionName);
-        resolve({ client, collection });
-      })
-      .catch(reason => {
-        reject(reason);
-      });
-  });
-}
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
