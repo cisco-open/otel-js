3bbf434de187d30b1e164a7d400714bcad5e8f24
haddasb <haddasb@cisco.com>
2022-03-15 14:36:07 +0200

    fix(sqs-receive-msg): add case of multiple msgs

--- test/instrumentatios/extentions/aws_sdk_v3.test.ts
+++ test/instrumentatios/extentions/aws_sdk_v3.test.ts
@@ -367,8 +367,34 @@ describe('Test AWS V3 with nock', () => {
         spans[0].attributes['aws.sqs.record.message_id'],
         '45bfb285-7169-40b0-9de8-c72052bdfe90'
       );
-      // done();
-      // }
+    });
+
+    it('sqs receive more than one message - promise await', async () => {
+      nock(`https://sqs.${region}.amazonaws.com/`)
+        .post('/')
+        .reply(
+          200,
+          fs.readFileSync(
+            `${__dirname}/aws-mock-responses/sqs-receive-message-more-than-one.xml`,
+            'utf8'
+          )
+        );
+      const params = {
+        AttributeNames: ['SentTimestamp', 'SenderId'],
+        MaxNumberOfMessages: 10,
+        MessageAttributeNames: ['All'],
+        QueueUrl: 'queue/dummy-account/testing',
+        VisibilityTimeout: 20,
+        WaitTimeSeconds: 0,
+      };
+      await sqsClient.receiveMessage(params);
+      const spans = memoryExporter.getFinishedSpans();
+      console.log(spans);
+      assert.strictEqual(spans.length, 1);
+      assert.strictEqual(
+        spans[0].attributes['aws.sqs.record'],
+        '[{"MessageId":"7fe3a2a6-c36a-420a-9830-edba7747f61a","ReceiptHandle":"AQEBo/8PlpWUiTd7Ks5xJRlbQ0yfHUgRkzSaYlsW9m4VDqIvJvGcr8ZOSuuQH3ChvDouB9xR1CdeSijDoRvoiePU7lx32+K/s1ZpascBwpdVO58R54Se6ak5pn3c/5giJ+8ZVMFqzRSCW0zw9dPGFvVhV16rltqpbffgVwboWmTkwiMvyon7aZUexuzqYwuqOVS21PfuROfEOSCvqcM5WMROoadIJsUgLbOyuV9O5yB4Wp8eva9ZPCNIvex6kj8duiSFjkW7hdlDR3FuGu7TQbLpCaFm3HnDmYD694FrP1WQQUhw6KjGvmSlPSTkkKkz6CWbHSw67RdMekRrzhodWWEj8Zls7rJXEtWxQDpU5bq2EMpbSpv7ofQ7dHOYI3w0iIhSJglr6TVva/0BNwZhuKSZAQ==","MD5OfBody":"2a6d3c908467b6ca7d6a820217913613","Body":"msg 3","Attributes":{"SentTimestamp":"1647343166484","SenderId":"AROA4I6NZPZGDWDPWYSYK:haddasb@cisco.com"}},{"MessageId":"ba865c18-8146-414c-99b0-b4ab1fc5c14e","ReceiptHandle":"AQEBtxF0wtdEgZ7qvzDbDJ+fxZwMJrwI/NVpzChuHTzBvGhjasTcX0HjS4poZYH2zvc+QiKWZ318pgkIt91ufkoXXkMhC3jv+nmV9nOS5hWJPUbeeN97NuTl1F1ArSYoMaVH8Q077iLsKuTmvB3ROALCMF9mvjBBkTWcby2nlShQZLGIG8G09VE19M5MRfHwntsjnclrK7mitcM+0HLq90zyiuDdGKjin9ozjP1Tx8768L+Xjx6YJXMGePy2qveW6tzI1Q5i44Jy0b4z1Bn08DpgPhcZs+V9SnU+AtGKxlDtkX5bq9H2huwlrMxMSgbXpM1pzyLieE1w2ScS+lLhyrZl1FFHGpl2pLHQ94TwbFCzISUup1j+QSQdaVQuoyt6pWLhtlxoqgHpI5cmuzTgEniHrQ==","MD5OfBody":"49957fc82eea4500cb738d507b094594","Body":"msg 2","Attributes":{"SentTimestamp":"1647342798650","SenderId":"AROA4I6NZPZGDWDPWYSYK:haddasb@cisco.com"}}]'
+      );
     });
   });
 });
