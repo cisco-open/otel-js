9824dd028d2ad64f9571de211d62038deda71475
Osher Vaknin <osherv@cisco.com>
2022-05-01 19:36:52 +0300

    fix(index): fixed index

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+
 export * as opentelemetry from '@opentelemetry/api';
 export * as ciscoTracing from './tracing';
 export { Options } from './options';
