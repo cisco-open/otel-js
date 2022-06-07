bd8938c31cc7b97a3a6dedc8869792b3ca3cf562
Osher Vaknin <osherv@cisco.com>
2022-06-07 12:28:25 +0300

    feat(e2e-tests): test1

--- system-tests/express/server/server.js
+++ system-tests/express/server/server.js
@@ -1,24 +1,5 @@
 'use strict';
 
-const { ciscoTracing } = require('cisco-telescope');
-const api = require('@opentelemetry/api');
-
-api.diag.setLogger(new api.DiagConsoleLogger(), api.DiagLogLevel.ALL);
-
-const userOptions = {
-  debug: true,
-  serviceName: 'my-app-name',
-  ciscoToken: 'fso-token',
-  exporters: [
-    {
-      collectorEndpoint: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
-      type: 'otlp-grpc',
-    },
-  ],
-};
-
-const provider = ciscoTracing.init(userOptions);
-
 require('http');
 const express = require('express');
 
@@ -32,5 +13,4 @@ app.get('/', (req, res) => {
   res.status(200).send('Hallo Worldz');
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(PORT, () => console.log(`Running on http://${HOST}:${PORT}`));
---
+++ system-tests/express/server/tracing.js
@@ -0,0 +1,19 @@
+
+const { ciscoTracing } = require('cisco-telescope');
+const api = require('@opentelemetry/api');
+
+api.diag.setLogger(new api.DiagConsoleLogger(), api.DiagLogLevel.ALL);
+
+const userOptions = {
+  debug: true,
+  serviceName: 'my-app-name',
+  ciscoToken: 'fso-token',
+  exporters: [
+    {
+      collectorEndpoint: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
+      type: 'otlp-grpc',
+    },
+  ],
+};
+
+const provider = ciscoTracing.init(userOptions);
