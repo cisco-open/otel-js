5aec6fbb6094dd716ebf3601cdce0a40e53beede
Osher Vaknin <osherv@cisco.com>
2022-06-07 16:50:03 +0300

    feat(e2e-tests): test1

--- system-tests/express/test_client/client.js
+++ system-tests/express/test_client/client.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const got = require('got');
-got(process.env.SERVER_URL, {
+got(process.env.SERVER_URL ?? "http://0.0.0.0:7080", {
   retry: {
     errorCodes: ['ECONNREFUSED'],
     maxRetryAfter: 1000,
